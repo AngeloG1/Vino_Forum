43916aa7ab205137274f7a8303d0b334abb0e304
AngeloG1 <galasso.angelo@icloud.com>
2015-02-25 15:38:22 -0500

    fix style sheets

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,6 +1,10 @@
 require 'active_record'
 require 'pry'
 
-ActiveRecord::Base.establish_connection(:adapter  => "postgresql",:host     => "localhost",:username => "AngeloMacBook",:database => "project_one")
+ActiveRecord::Base.establish_connection(
+	:adapter  => "postgresql",
+	:host => "localhost",
+	:username => "AngeloMacBook",
+	:database => "project_one")
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
--- server.rb
+++ server.rb
@@ -7,14 +7,14 @@ require './lib/connection.rb'
 require './lib/class.rb'
 
 #This will be the home page html '/' 
- get '/' do
- 	
+get '/' do
+	
 
- 	File.read('./views/directory.html')
- 	topic_name = Topic.last(5)
- 	topic_description = Topic.select(:description)
+	File.read('./views/directory.html')
+	topic_name = Topic.last(5)
+	topic_description = Topic.select(:description)
 
-Mustache.render(File.read('./views/directory.html'),{
+	Mustache.render(File.read('./views/directory.html'),{
 		name: topic_name[0][:name], description: topic_name[0][:description], date: topic_name[0][:date],
 		name2: topic_name[1][:name], description2: topic_name[1][:description], date2: topic_name[1][:date],
 		name3: topic_name[2][:name], description3: topic_name[2][:description], date3: topic_name[2][:date],
@@ -44,84 +44,49 @@ end
 
 get '/new/post' do
 	File.read('./views/post_create.html')
+	
+	topic_name = Topic.last(50) 
+	list_of_topics = []
 
- #    array = []
+	topic_name.each do |name|
+		list_of_topics.push(name.name)
+	end 
 
-	# Topic.find_each do |name|
- # 	array.push(name.name)                                    
- # 	end    
-topic_name = Topic.last(50) 
-list_of_topics = []
-
-topic_name.each do |name|
-	list_of_topics.push(name.name)
-end 
-
-Mustache.render(File.read('./views/post_create.html'),{ name: list_of_topics})
-		# name: topic_name[0][:name],
-		# name2: topic_name[1][:name],
-		# name3: topic_name[2][:name],
-		# name4: topic_name[3][:name], 
-		# })
+	Mustache.render(File.read('./views/post_create.html'),{ name: list_of_topics})
+	
 end 
 
 post '/post' do
-	 topic = params["topic"] 
-	 title = params["name"]
-	 message = params["message"]
-	 author = params["author"]
-	 date = params["date"]
-	 likes = 0
+	topic = params["topic"] 
+	title = params["name"]
+	message = params["message"]
+	author = params["author"]
+	date = params["date"]
+	likes = 0
 
-Post.create(cat_id: topic,title: title,message: message,author: author,date: date,likes: likes)
+	Post.create(cat_id: topic,title: title,message: message,author: author,date: date,likes: likes)
 
 
-redirect '/'
+	redirect '/'
 end 
 
 get '/categories' do
 	all_topics = Topic.all.to_a
 	all_topics_name = []
-     
-    all_topics.each do |name|
-    	all_topics_name.push(name.name)
-    end
+	
+	all_topics.each do |name|
+		all_topics_name.push(name.name)
+	end
 
 	Mustache.render(File.read('./views/categories_all.html'),{ name: all_topics_name})
 
 end
 
 get '/:topic' do 
-	finally=[]
 	topic = params[:topic]
-	topic_info = Post.where(cat_id: topic)
-
-	topic_info.each do |list_of_post| 
-		finally.push(list_of_post.message)
-	end 
+	topic_info = Post.where(cat_id: topic).to_a
 
 
-Mustache.render(File.read('./views/topic_page.html'), {info: finally})
+	Mustache.render(File.read('./views/topic_page.html'), {info: topic_info})
 
 end 
-
-# File.read('house.html')
-
-# name = params[:name]
-# houses = House.find_by(name: name)
-
-
-# Mustache.render(File.read('house.html'),{id: houses.id, name: houses.name, sigil_url: houses.sigil_url, region: houses.region})
-
-# get '/:name' do 
-
-# 	File.read('house.html')
-
-# 	name = params[:name]
-
-# 	houses = House.find_by(name: name)
-
-
-# 	Mustache.render(File.read('house.html'),{id: houses.id, name: houses.name, sigil_url: houses.sigil_url, region: houses.region})
-
-# end 
